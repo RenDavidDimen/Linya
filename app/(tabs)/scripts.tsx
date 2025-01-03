63829b17388a1a24798bca30f032b39f24ca1f56
RenDavidDimen <rendaviddimen@gmail.com>
2025-01-03 16:02:28 -0500

    update script page layout and include sample script scrolling

--- app/(tabs)/scripts.tsx
+++ app/(tabs)/scripts.tsx
@@ -1,9 +1,40 @@
 import { Text, View, StyleSheet } from "react-native";
+import * as Colors from "@/constants/Colors";
+import * as Test from "@/constants/TestData";
+import Button from "@/components/Button";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function Scripts() {
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.scriptContainer}>
+        <Text style={styles.scriptTitle}>{item.title} </Text>
+        <Text style={styles.scriptCharacter}>{item.character}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Behold the SCRIPTS screen of Linya!</Text>
+      {/* only show this line if no scripts are saved within the application */}
+      <Text style={styles.text}>
+        Press the 'Add Script' below to get started!
+      </Text>
+      <FlatList
+        data={Test.scriptData}
+        renderItem={renderItem}
+        onRefresh={() => console.log("refreshing")}
+      />
+      <Button
+        color={Colors.BLUE}
+        label="Add Script"
+        onPress={() => alert("Adding Script")}
+      />
+      <Button
+        color={Colors.WHITE}
+        label="Edit Script"
+        onPress={() => alert("Editing Script")}
+      />
     </View>
   );
 }
@@ -13,9 +44,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#2B323B",
+    backgroundColor: Colors.GREY,
   },
   text: {
-    color: "#F6FAF0",
+    color: Colors.WHITE,
+  },
+  scriptContainer: {
+    color: Colors.GREY,
+    backgroundColor: Colors.WHITE,
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    margin: 10,
+    borderRadius: 10,
+  },
+  scriptTitle: {
+    fontSize: 32,
+    fontWeight: 600,
+  },
+  scriptCharacter: {
+    fontSize: 18,
   },
 });
