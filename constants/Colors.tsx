351f55ec5ad5100892a59461bbd992e93d8dd451
RenDavidDimen <rendaviddimen@gmail.com>
2025-01-03 16:01:40 -0500

    set up constant files for consistently used values

---
+++ constants/Colors.tsx
@@ -0,0 +1,5 @@
+export const RED = "#F33B88";
+export const BLUE = "#6D95FE";
+export const LIGHTGREY = "#353A44";
+export const GREY = "#2B323B";
+export const WHITE = "#F6FAF0";
---
+++ constants/TestData.tsx
@@ -0,0 +1,44 @@
+export const scriptData = [
+  {
+    key: "A",
+    lastOpened: 1735935012,
+    title: "The Fourth",
+    character: "Mark",
+  },
+  {
+    key: "B",
+    lastOpened: 1735945012,
+    title: "The Fifth",
+    character: "David",
+  },
+  {
+    key: "C",
+    lastOpened: 1736035012,
+    title: "The Minor Fall",
+    character: "Haleigh",
+  },
+  {
+    key: "D",
+    lastOpened: 1736135012,
+    title: "The Major Lift",
+    character: "Lou",
+  },
+  {
+    key: "E",
+    lastOpened: 1736235012,
+    title: "The Baffled King",
+    character: "Ya",
+  },
+  {
+    key: "F",
+    lastOpened: 1736335012,
+    title: "Composing",
+    character: "HERO",
+  },
+  {
+    key: "G",
+    lastOpened: 1736435012,
+    title: "American Excess",
+    character: "Aziz",
+  },
+];
